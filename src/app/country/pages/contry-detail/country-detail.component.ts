6646408196d6ff3de13c475b4b33e3ecf5a07cf3
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-03-30 14:20:59 +0200

    RxJs switchMap example implementation

--- src/app/country/pages/contry-detail/country-detail.component.ts
+++ src/app/country/pages/contry-detail/country-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
 import { CountryService } from '../../services/country.service';
 
 @Component({
@@ -16,14 +18,24 @@ export class CountryDetailComponent implements OnInit {
     ) { }
 
   ngOnInit( ): void {
+    //With RxJs switchMap operator
     this.activatedRoute.params
-      .subscribe( ({ id })  => {
-        // console.log(id);
-        this.countryServiceInstance.requestCountryByCode(id)
-          .subscribe( country => {
-            console.log(country);
-          });
+      .pipe(
+          switchMap(({id}) => this.countryServiceInstance.requestCountryByCode( id ))
+      )
+      .subscribe( resp => {
+        console.log(resp);
       });
+
+    //Without RxJs operators 
+    // this.activatedRoute.params
+    //   .subscribe( ({ id })  => {
+    //     // console.log(id);
+    //     this.countryServiceInstance.requestCountryByCode(id)
+    //       .subscribe( country => {
+    //         console.log(country);
+    //       });
+    //   });
   }
 
 }
