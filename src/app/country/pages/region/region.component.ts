70b04e31445eba3ce07edcac73f1ebf4e19ba76f
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-04-05 23:04:53 +0200

    first approach to CSS conditional classes

--- src/app/country/pages/region/region.component.ts
+++ src/app/country/pages/region/region.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-region',
   templateUrl: './region.component.html',
   styles: [
+    `button {
+        margin-right: 10px;
+    }`
   ]
 })
 export class RegionComponent {
 
+  public regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  public activeRegion: string = '';
+
   constructor() { }
+
+  setActiveRegion(region: string) {
+    this.activeRegion = region;
+    //TODO -> Call Service
+  }
 
 }
